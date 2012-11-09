64758016f461df0ffd416d526e3d56bd615f0ecd
resteve <resteve@zikzakmedia.com>
2012-11-10 00:22:32 +0100

    Initial release

---
+++ doc/index.rst
@@ -0,0 +1,5 @@
+Account Invoice Price List Module
+#################################
+
+The account_invoice_price_list module add in "on_change_product" the price with 
+calculated by price list.
---
+++ invoice.py
@@ -0,0 +1,24 @@
+#This file is part account_invoice_price_list module for Tryton.
+#The COPYRIGHT file at the top level of this repository contains 
+#the full copyright notices and license terms.
+from trytond.pool import Pool, PoolMeta
+from trytond.transaction import Transaction
+
+__all__ = ['InvoiceLine']
+__metaclass__ = PoolMeta
+
+class InvoiceLine:
+    'Invoice Line'
+    __name__ = 'account.invoice.line'
+
+    def on_change_product(self):
+        Product = Pool().get('product.product')
+        res = super(InvoiceLine, self).on_change_product()
+        if self.invoice.party.sale_price_list:
+            with Transaction().set_context({
+                    'price_list':self.invoice.party.sale_price_list,
+                    'customer': self.invoice.party.id,
+                }):
+                res['unit_price'] = Product.get_sale_price([self.product],
+                self.quantity or 0)[self.product.id]
+        return res
