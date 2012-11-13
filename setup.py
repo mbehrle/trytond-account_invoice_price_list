a201d10dfc8ee0ec751d277f2b1a40933fcc682b
resteve <resteve@zikzakmedia.com>
2012-11-13 14:43:18 +0100

    Setup. Rename package to trytonzz

--- setup.py
+++ setup.py
@@ -27,20 +27,20 @@ for dep in info.get('depends', []):
 requires.append('trytond >= %s.%s, < %s.%s' %
         (major_version, minor_version, major_version, minor_version + 1))
 
-setup(name='trytond_account_invoice_price_list',
+setup(name='trytonzz_account_invoice_price_list',
     version=info.get('version', '0.0.1'),
     description='Tryton module to apply price list in invoices',
     author='Zikzakmedia SL',
     author_email='zikzak@zikzakmedia.com',
     url='http://www.zikzakmedia.com',
-    download_url="https://bitbucket.org/zikzakmedia/trytond-account_invoice_price_list",
-    package_dir={'trytond.modules.account_invoice_price_list': '.'},
+    download_url="https://bitbucket.org/zikzakmedia/trytonzz-account_invoice_price_list",
+    package_dir={'trytonzz.modules.account_invoice_price_list': '.'},
     packages=[
-        'trytond.modules.account_invoice_price_list',
-        'trytond.modules.account_invoice_price_list.tests',
+        'trytonzz.modules.account_invoice_price_list',
+        'trytonzz.modules.account_invoice_price_list.tests',
         ],
     package_data={
-        'trytond.modules.account_invoice_price_list': info.get('xml', []) \
+        'trytonzz.modules.account_invoice_price_list': info.get('xml', []) \
             + ['tryton.cfg', 'locale/*.po'],
         },
     classifiers=[
@@ -63,9 +63,9 @@ setup(name='trytond_account_invoice_price_list',
     install_requires=requires,
     zip_safe=False,
     entry_points="""
-    [trytond.modules]
-    account_invoice_price_list = trytond.modules.account_invoice_price_list
+    [trytonzz.modules]
+    account_invoice_price_list = trytonzz.modules.account_invoice_price_list
     """,
     test_suite='tests',
-    test_loader='trytond.test_loader:Loader',
+    test_loader='trytonzz.test_loader:Loader',
 )
