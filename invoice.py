19be039c89ce8c3bbe6ad18b1330b5c0db118726
resteve <resteve@zikzakmedia.com>
2014-07-08 18:42:29 +0200

    invoice standalone create invoice lines without relate invoice

--- invoice.py
+++ invoice.py
@@ -14,7 +14,7 @@ class InvoiceLine:
     def on_change_product(self):
         Product = Pool().get('product.product')
         res = super(InvoiceLine, self).on_change_product()
-        if self.invoice.party and self.invoice.party.sale_price_list and \
+        if self.invoice and self.invoice.party and self.invoice.party.sale_price_list and \
                 self.invoice.type in ['out_invoice', 'out_credit_note'] and \
                 self.product:
             with Transaction().set_context({
