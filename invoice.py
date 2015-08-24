ef041dd18fec93eb60d4c8e66a477c12c1f1165e
resteve <resteve@zikzakmedia.com>
2015-08-24 22:33:34 +0200

    New API for on_change

--- invoice.py
+++ invoice.py
@@ -2,6 +2,7 @@
 #The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
 from trytond.pool import Pool, PoolMeta
+from trytond.model import fields
 from trytond.transaction import Transaction
 
 __all__ = ['InvoiceLine']
@@ -9,19 +10,21 @@ __metaclass__ = PoolMeta
 
 
 class InvoiceLine:
-    'Invoice Line'
     __name__ = 'account.invoice.line'
 
+    @fields.depends('party', 'invoice')
     def on_change_product(self):
         Product = Pool().get('product.product')
-        res = super(InvoiceLine, self).on_change_product()
+
+        super(InvoiceLine, self).on_change_product()
         party = self.party or self.invoice.party
+
         if (party and party.sale_price_list and self.product
                 and self.invoice.type in ['out_invoice', 'out_credit_note']):
             with Transaction().set_context({
                     'price_list': party.sale_price_list.id,
                     'customer': party.id,
                 }):
-                res['unit_price'] = Product.get_sale_price([self.product],
-                self.quantity or 0)[self.product.id]
-        return res
+
+                prices = Product.get_sale_price([self.product], self.quantity or 0)
+                self.unit_price = prices[self.product.id]
