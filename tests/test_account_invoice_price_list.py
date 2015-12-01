f57115f245dd4696488b07c37e70eed8d37be047
resteve <resteve@zikzakmedia.com>
2015-12-01 10:58:04 +0100

    Tests

--- tests/test_account_invoice_price_list.py
+++ tests/test_account_invoice_price_list.py
@@ -1,21 +1,14 @@
-#!/usr/bin/env python
-# This file is part account_invoice_price_list module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the account_invoice_price_list module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class AccountInvoicePriceListTestCase(unittest.TestCase):
+class AccountInvoicePriceListTestCase(ModuleTestCase):
     'Test Account Invoice Price List module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('account_invoice_price_list')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'account_invoice_price_list'
 
 
 def suite():
