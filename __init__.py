33b01a94f014c31465338a71b3e08ed37a2d8b2f
Albert Cervera i Areny <albert@nan-tic.com>
2021-03-27 23:31:01 +0100

    Pyflakes fixes.

--- __init__.py
+++ __init__.py
@@ -1,11 +1,9 @@
-#This file is part account_invoice_price_list module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
+#The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
 from trytond.pool import Pool
-from .invoice import *
-
+from . import invoice
 
 def register():
     Pool.register(
-        InvoiceLine,
+        invoice.InvoiceLine,
         module='account_invoice_price_list', type_='model')
